24684eeca7f4c20544d7da3ea6044e472c258a23
Pratim Ghosh <pratim7674ghosh@gmail.com>
2024-08-25 05:27:02 +0000

    dataTypes

---
+++ 01_basics/01_dartatypes.js
@@ -0,0 +1,16 @@
+let name ="Pratim"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+// BigInt 
+// string => ""
+// Boolean => true/false
+// null =standalone value
+// undefined =>
+// Symbol => unique
+
+
+//object
+
+console.log(typeof age);
