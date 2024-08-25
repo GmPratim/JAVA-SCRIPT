4e42b4cf9a03f17517b4e4c66368c0d43d80d70d
Pratim Ghosh <pratim7674ghosh@gmail.com>
2024-08-25 05:27:31 +0000

    var

---
+++ 01_basics/01_var1.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = "only.gmail.com"
+var accountPass = "564789"
+let exampel;
+accountCity = "kolkata"
+console.table([accountId,accountEmail,accountPass,accountCity.exampel])
+
